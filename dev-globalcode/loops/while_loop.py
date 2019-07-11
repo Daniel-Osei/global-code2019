60c85f6f5cf01b0de79b682a595fe562028038a8
Daniel-Osei <demajesty.6@gmail.com>
2019-07-11 12:42:41 +0100

    fixed reversed_even

--- dev-globalcode/loops/while_loop.py
+++ dev-globalcode/loops/while_loop.py
@@ -3,8 +3,8 @@ print ("printing from 1 - 10 with while")
 
 i = 0
 while(i < 10):
-	i = i + 1
-	print (i)
+    i = i + 1
+    print (i)
 
 input("press enter to continue")
 #try printing numbers from 7 - 19
@@ -12,8 +12,8 @@ print ("print from 7 - 19")
 
 j = 6
 while(j < 19):
-	j = j + 1
-	print (j)
+    j = j + 1
+    print (j)
 
 input("press enter to continue")
 
@@ -22,8 +22,8 @@ print ("print even numbers between 12 and 20 ")
 
 k = 12
 while(k < 18):
-	k = k + 2
-	print (k)
+    k = k + 2
+    print (k)
 
 input("press enter to continue")
 
@@ -31,19 +31,19 @@ input("press enter to continue")
 
 
 def even():
-	start = int(input("Enter the lower limit: "))
-	end = int(input("Enter the upper limit: "))
-	for num in range(start, end + 1):
-		if num % 2 == 0:
-			print (num)
+    start = int(input("Enter the lower limit: "))
+    end = int(input("Enter the upper limit: "))
+    for num in range(start, end + 1):
+        if num % 2 == 0:
+            print (num)
 
 even()
 
 def reverse_even():
-	start = int(input("Enter the lower limit: "))
-	end = int(input("Enter the upper limit: "))
-	for num in range(start, end + 1):
-		if num % 2 == 0:
-			print (reversed(num))
+    start = int(input("Enter the lower limit: "))
+    end = int(input("Enter the upper limit: "))
+    for num in range(end, start - 1, -1):
+        if num % 2 == 0:
+            print (num)
 
-#reverse_even()
+reverse_even()
